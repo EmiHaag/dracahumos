1ba74d7a563540e4e04649cff760bae0316fd52b
Emiliano Haag <emilianohaag10@gmail.com>
2022-11-25 00:42:59 -0300

    Se corrigió subida imágenes en backend que mostraba nombre archivo blob. Se agregaron mensajes a las subidas y se corrigieron limites de peso de archivos en el backend.

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -5,11 +5,10 @@ function AlertDismissible({ heading, msg, setShowAlert, show, colorBg }) {
   if (show) {
     return (
       <Alert
-        style={{ position: "absolute", bottom: "2em", right: "3em" }}
+        /*  style={{ position: "absolute", bottom: "2em", right: "3em" }} */
         variant={colorBg}
         onClose={() => setShowAlert(false)}
-        dismissible
-      >
+        dismissible>
         <Alert.Heading>{heading}</Alert.Heading>
         <p>{msg}</p>
       </Alert>
--- src/screens/admin/imagesUploader/index.jsx
+++ src/screens/admin/imagesUploader/index.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { Card, Button, Modal, Container, Row } from "react-bootstrap";
+import { Card, Col, Button, Modal, Container, Row } from "react-bootstrap";
 import TableImages from "./TableImages";
-import Compressor from 'compressorjs';
-
+import Compressor from "compressorjs";
+import AlertDismissible from "../../../components/alert";
 import { consts } from "../config";
 
 const ImagesUploader = (props) => {
   /*   let { selectedImages, setSelectedImages, indexImage } = props; */
-
+  const [msgAlert, setMsgAlert] = useState({
+    headingAlert: "",
+    msgAlert: "",
+  });
+  const [showAlert, setShowAlert] = useState(false);
   const [f, setFile] = useState({});
   const [imagesInServer, setImagesInServer] = useState();
   const axios = require("axios");
@@ -17,10 +21,10 @@ const ImagesUploader = (props) => {
       .get(consts.LOCALHOST + "backend/images/getImagesFromServer.php")
       .then((res) => {
         setImagesInServer(res.data);
-        console.log(res.data);
+        // console.log(res.data);
       })
       .catch(function (error) {
-        console.log(error);
+        //  console.log(error);
       });
   };
 
@@ -29,40 +33,59 @@ const ImagesUploader = (props) => {
   }, []);
 
   const fileSelectedHandler = (file) => {
-    
     // const image = e.target.files[0];
+
+    if (file.name.split(".").pop() === "mp4") {
+      setFile(file);
+      return;
+    }
     new Compressor(file, {
       quality: 0.6, // 0.6 can also be used, but its not recommended to go below.
       success: (compressedResult) => {
         // compressedResult has the compressed file.
-        // Use the compressed file to upload the images to your server.        
-        console.log(compressedResult)
-        setFile(compressedResult)
+        // Use the compressed file to upload the images to your server.
+        // console.log(compressedResult, file);
+        setFile(compressedResult);
+        //  console.log("archivo subido: ", f);
       },
     });
-    
-    
-    
-    
+
     //setFile(file);
   };
 
   const uploadFile = async () => {
     const fd = new FormData();
     fd.append("image", f);
-    console.log(f);
+    //console.log(f);
+    setMsgAlert({
+      headingAlert: "Subiendo archivo, aguarde..",
+      msgAlert: "",
+      bgColor: "warning",
+    });
+    setShowAlert(true);
     await axios
       .post(consts.LOCALHOST + "backend/images/uploadSingleImage.php", fd)
       .then((res) => {
-        if (res.data.success) {
-          console.log("archivo subido correctamente, actualizando tabla..");
-          getImagesFromServer();
-        }
+        //console.log("archivo subido correctamente, actualizando tabla..");
+        getImagesFromServer();
+
         console.log(res.data);
+        setMsgAlert({
+          headingAlert: "Servidor:",
+          msgAlert: res.data,
+          bgColor: "warning",
+        });
+        setShowAlert(true);
       })
       .catch(function (error) {
         // handle error
-        console.log(error);
+        //console.log(error);
+        setMsgAlert({
+          headingAlert: "Ocurrió un error al intentar subir el archivo",
+          msgAlert: error,
+          bgColor: "danger",
+        });
+        setShowAlert(true);
       })
       .then(function () {
         // always executed
@@ -83,12 +106,15 @@ const ImagesUploader = (props) => {
     props.setSelectedImages(tempImgSelected, props.indexImage);
   };
 
-  const callback = (imageSelected)=>{
+  const callback = (imageSelected) => {
     tempImgSelected = imageSelected;
-  }
+  };
   return (
     <>
-      <Button variant="primary" onClick={handleShow} style={{ width: "100%", fontSize:"0.8em" , padding:"0"}}>
+      <Button
+        variant="primary"
+        onClick={handleShow}
+        style={{ width: "100%", fontSize: "0.8em", padding: "0" }}>
         Imagen/video {currentIndImage + 1}
       </Button>
       <Modal show={show} onHide={handleClose} size="lg">
@@ -101,17 +127,29 @@ const ImagesUploader = (props) => {
               <input
                 type="file"
                 onChange={(e) => fileSelectedHandler(e.target.files[0])}
-                accept="image/*"
+                accept="image/*, video/*"
               />
             </Card>
-            <Button onClick={uploadFile}>Subir</Button>
+            <Row>
+              <Col>
+                <Button onClick={uploadFile}>Subir</Button>
+              </Col>
+              <Col>
+                <AlertDismissible
+                  heading={msgAlert.headingAlert}
+                  msg={msgAlert.msgAlert}
+                  setShowAlert={setShowAlert}
+                  show={showAlert}
+                  colorBg={msgAlert.bgColor}
+                />
+              </Col>
+            </Row>
 
             <Row>
               <TableImages
                 images={imagesInServer}
                 /* handleImageSelected={tempImgSelected} */
-                onClick={callback}
-              ></TableImages>
+                onClick={callback}></TableImages>
             </Row>
           </Container>
         </Modal.Body>
